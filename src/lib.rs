9a3f6c2f6e3182fe6e434c1cb81e07d172d04cae
kuqmua <serezhashmakov@outlook.com>
2023-06-25 06:11:04 +0300

    into_actix_web_http_response proc macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
-#[proc_macro_derive()]
-pub fn (input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    proc_macro_helpers::panic_location::panic_location();//panic_location function from https://github.com/kuqmua/proc_macro_helpers
-    let ast: syn::DeriveInput =
-        syn::parse(input).unwrap_or_else(|_| panic!("let ast: syn::DeriveInput = syn::parse(input) failed"));
+#[proc_macro_derive(IntoActixWebHttpResponse)]
+pub fn into_actix_web_http_response(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    proc_macro_helpers::panic_location::panic_location(); //panic_location function from https://github.com/kuqmua/proc_macro_helpers
+    let ast: syn::DeriveInput = syn::parse(input)
+        .unwrap_or_else(|_| panic!("let ast: syn::DeriveInput = syn::parse(input) failed"));
     let ident = &ast.ident;
-    match ast.data {
-        syn::Data::Union(_) => panic!("does not work on union!"),
-        syn::Data::Struct(_) => panic!("does not work on structs!"),
-        syn::Data::Enum(_) => panic!("does not work on enums!"),
-    }
-    let gen = quote::quote! {};
+    let gen = quote::quote! {
+        impl From<#ident> for actix_web::HttpResponse {
+            fn from(val: #ident) -> Self {
+                let mut actix_web_http_response: actix_web::HttpResponseBuilder = (&val).into();
+                actix_web_http_response.json(actix_web::web::Json(val))
+            }
+        }
+    };
     //println!("{gen}");
     gen.into()
 }
