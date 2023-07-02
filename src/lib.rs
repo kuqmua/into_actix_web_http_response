763a3396d5816ca4b7aec100c664112f7bf36a26
kuqmua <serezhashmakov@outlook.com>
2023-07-02 05:13:06 +0300

    pub static AST_PARSE_FAILED: &str = "let ast: syn::DeriveInput = syn::parse(input) failed";

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 #[proc_macro_derive(IntoActixWebHttpResponse)]
 pub fn into_actix_web_http_response(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro_helpers::panic_location::panic_location(); //panic_location function from https://github.com/kuqmua/proc_macro_helpers
-    let ast: syn::DeriveInput = syn::parse(input)
-        .unwrap_or_else(|_| panic!("let ast: syn::DeriveInput = syn::parse(input) failed"));
+    let ast: syn::DeriveInput = syn::parse(input).unwrap_or_else(|_| {
+        panic!(
+            "{}",
+            proc_macro_helpers::global_variables::hardcode::AST_PARSE_FAILED
+        )
+    });
     let ident = &ast.ident;
     let gen = quote::quote! {
         impl From<#ident> for actix_web::HttpResponse {
